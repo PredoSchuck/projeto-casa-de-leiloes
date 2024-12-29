0f6db8ca9721c38c8a1f64f6fe7066115e8d7b3f
Predo Schuck <pedro_augusto.2@hotmail.com>
2024-12-29 17:37:53 -0300

    Limpando comentarios não necessarios

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -1,19 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
 
 public class ProdutosDAO {
     
@@ -39,4 +28,3 @@ public class ProdutosDAO {
     
         
 }
-
